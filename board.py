4cffe2b2794f0e9db6e19b10da56c7109d98488e
Luis Viveiros <luisviveiros.official@gmail.com>
2021-04-15 18:04:44 +0000

    Add new piece Update

--- board.py
+++ board.py
@@ -32,8 +32,8 @@ class Board():
         self.PIECE_I = [[0,0],[1,0],[2,0],[3,0]]
         self.PIECE_O = [[0,0],[0,1],[1,0],[1,1]]
         self.PIECE_T = [[0,1],[1,0],[1,1],[1,2]]
-        self.PIECE_Z = [[0,1],[0,2],[1,0],[1,1]]
-        self.PIECE_L = [[0,0],[0,1],[1,1],[1,2]]
+        self.PIECE_Z = [[0,0],[0,1],[1,1],[1,2]]
+        self.PIECE_L = [[0,0],[0,1],[0,2],[1,2]]
 
 
         self.pieces = [self.PIECE_I, self.PIECE_O, self.PIECE_T, self.PIECE_Z, self.PIECE_L]
@@ -85,13 +85,18 @@ class Board():
         new_piece = Piece(random.choice(self.pieces))
         self.active_piece = new_piece
         self.draw_shape(1, self.active_piece)
-
         self.print_board()
 
     def tick(self):
-        print("tick")
+
         self.draw_shape(0, self.active_piece)
         self.active_piece.move_down()
         self.draw_shape(1, self.active_piece)
 
+        for pos in self.active_piece.shape_positions:
+            if pos[0] >= 20:
+                self.place_new_piece()
+                break
+
+        print("/////////////////////////////////////////////////////////")
         self.print_board()
