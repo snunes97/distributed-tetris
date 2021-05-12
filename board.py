7a7041e15906335c24a0f28b8598950b51ed1a5a
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2021-05-12 23:34:41 +0100

    Revert "jjj"
    This reverts commit 06bcb7a646295a6caeb80bd1c736a1dac4c0f5ea.

--- board.py
+++ board.py
@@ -3,7 +3,9 @@ import copy
 from piece import Piece
 
 class Board():
-    def __init__(self):
+    def __init__(self, gui):
+
+        self.gui = gui
 
         #Defines the board
         self.board = [[0,0,0,0,0,0,0,0,0,0,0],
@@ -29,16 +31,14 @@ class Board():
                      [0,0,0,0,0,0,0,0,0,0,0]]
 
         #Defines the pieces
-            #7 Pieces
-        self.PIECE_I = [[0, 0], [0, 1], [0, 2], [0, 3]]
-        self.PIECE_O = [[0, 0], [0, 1], [1, 0], [1, 1]]
-        self.PIECE_T = [[0, 1], [1, 0], [1, 1], [1, 2]]
-        self.PIECE_Z = [[0, 0], [0, 1], [1, 1], [1, 2]]
-        self.PIECE_N = [[1, 0], [1, 1], [0, 1], [0, 2]]
-        self.PIECE_L = [[0, 0], [0, 1], [0, 2], [1, 2]]
-        self.PIECE_J = [[1, 0], [0, 0], [0, 1], [0, 2]]
+            #6 Pieces
+        self.PIECE_I = [[0,0],[1,0],[2,0],[3,0]]
+        self.PIECE_O = [[0,0],[0,1],[1,0],[1,1]]
+        self.PIECE_T = [[0,1],[1,0],[1,1],[1,2]]
+        self.PIECE_Z = [[0,0],[0,1],[1,1],[1,2]]
+        self.PIECE_L = [[0,0],[0,1],[0,2],[1,2]]
 
-        self.pieces = [self.PIECE_I, self.PIECE_O, self.PIECE_T, self.PIECE_Z, self.PIECE_N, self.PIECE_J, self.PIECE_L]
+        self.pieces = [self.PIECE_I, self.PIECE_O, self.PIECE_T, self.PIECE_Z, self.PIECE_L]
 
             #7 Colors
         self.shape_colors = [(0, 255, 255), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
@@ -52,20 +52,21 @@ class Board():
         for i in range(len(self.board)):
             print(self.board[i])
 
-    #confirma se a casa está ocupada e anda lateralmente (verificar redundancia)
     def try_move_left(self):
-        if self.active_piece.check_left(self.board) is True:
-            for pos in self.active_piece.shape_positions:
-                if pos[1] <= 0:
-                    return False
+
+        for pos in self.active_piece.shape_positions:
+            if pos[1] <= 0:
+                return False
+
         self.draw_shape(0, self.active_piece)
         self.active_piece.move_left()
 
     def try_move_right(self):
-        if self.active_piece.check_right(self.board) is True:
-            for pos in self.active_piece.shape_positions:
-                if pos[1] >= 10:
-                    return False
+
+        for pos in self.active_piece.shape_positions:
+            if pos[1] >= 10:
+                return False
+
         self.draw_shape(0, self.active_piece)
         self.active_piece.move_right()
 
@@ -103,14 +104,10 @@ class Board():
 
         #Confirma se não tem peça por baixo
         if self.active_piece.check_bellow(self.board):
-            for pos in self.active_piece.shape_positions:
-                if pos[0] == 0:
-                    break #endgame?
-
             self.draw_shape(2, self.active_piece)
             self.check_line()
             self.place_new_piece()
 
-
+        self.gui.update_gui(self.board)
         print("/////////////////////////////////////////////////////////")
         self.print_board()
--- dt_ui/ui/__main__.py
+++
@@ -1,6 +0,0 @@
-from stubs import PORT, HOST, GameServer
-from ui.client import Client
-
-game_server = GameServer(HOST, PORT)
-client = Client(game_server)
-client.try_enter()
