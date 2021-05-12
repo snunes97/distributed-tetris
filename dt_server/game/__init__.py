06bcb7a646295a6caeb80bd1c736a1dac4c0f5ea
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2021-05-12 23:30:27 +0100

    jjj
    jjj

---
+++ dt_server/game/__init__.py
@@ -0,0 +1,11 @@
+from game_server import GameServer
+from player import Player
+
+PORT = 5001
+HOST = "127.0.0.1"
+
+OP_VALIDATEPLAYER = "VALIDATEPLAYER"
+OP_MOVERIGHT = "MOVERIGHT"
+OP_MOVELEFT = "MOVELEFT"
+OP_ROTRIGHT = "ROTRIGHT"
+OP_ROTLEFT = "ROTLEFT"
--- timer.py
+++ timer.py
@@ -1,12 +1,18 @@
 from threading import Thread
 
-
 class Timer(Thread):
-    def __init__(self, event, board):
+    def __init__(self, event, board, gui):
         Thread.__init__(self)
         self.board = board
+        self.gui = gui
         self.stopped = event
+        self.firstRun = True
 
     def run(self):
+        if self.firstRun:
+            self.board.print_board()
+            self.gui.start()
+
         while not self.stopped.wait(1.0):
             self.board.tick()
+            self.gui.update_gui()
