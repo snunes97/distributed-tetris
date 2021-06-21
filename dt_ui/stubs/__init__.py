09675dbdcffa5a62aca8405475ef09388e7d7bee
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2021-06-21 17:58:23 +0100

    atualizações

--- dt_ui/stubs/__init__.py
+++ dt_ui/stubs/__init__.py
@@ -1,9 +1,14 @@
 from stubs.client_stub import ClientStub
 #25.67.247.53 - Testes com hamachi (WORKS!)
+
+# Ip dp cliente
 HOST = "25.67.247.53"
+
+# Portas dos sockets request-reply e publisher-subscriver
 PORT_REQREP = 5001
 PORT_PUBSUB = 5002
 
+# Comandos
 OP_VALIDATEPLAYER = "VALIDATEPLAYER"
 OP_MOVERIGHT = "MOVERIGHT"
 OP_MOVELEFT = "MOVELEFT"
@@ -13,3 +18,5 @@ OP_GETBOARD = "GETBOARD"
 OP_BOARDUPDATE = "UPDATEBOARD"
 OP_MATCHEXISTS = "MATCHEXISTS"
 OP_DISCONNECT = "DISCONNECT"
+
+# Quando o ClienteStub é criado o init executa o que está neste script.
